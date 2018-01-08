617889d6fc809ab880dc34af8c7d520866b284b8
Reddstar <ricardolucas1196@gmail.com>
2018-01-07 22:39:08 -0200

    Store and Achievements finished

--- app/src/main/java/com/ufrpe/cognit/cognitprototype/StudentMenuActivity.java
+++ app/src/main/java/com/ufrpe/cognit/cognitprototype/StudentMenuActivity.java
@@ -9,6 +9,9 @@ import android.widget.ImageButton;
 public class StudentMenuActivity extends AppCompatActivity {
 
     private ImageButton btnRanking;
+    private ImageButton btnClasses;
+    private ImageButton btnStore;
+    private ImageButton btnAchievements;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +19,35 @@ public class StudentMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         btnRanking = findViewById(R.id.btn_ranking);
+        btnClasses = findViewById(R.id.btn_achievements);
+        btnStore = findViewById(R.id.btn_store);
+        btnAchievements = findViewById(R.id.btn_achievements);
 
         btnRanking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent goRanking = new Intent(StudentMenuActivity.this, RankingActivity.class);
                 startActivity(goRanking);
+            }
+        });
+        btnClasses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goStore = new Intent(StudentMenuActivity.this, StoreActivity.class);
+                startActivity(goStore);
+            }
+        });
+        btnAchievements.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goAchievements = new Intent(StudentMenuActivity.this, AchievementsActivity.class);
+                startActivity(goAchievements);
             }
         });
     }
